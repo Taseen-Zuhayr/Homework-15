848f8d0c311c257b7612f9646c2055f6c807871b
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-11-01 20:04:10 +0600

    Homework 15

---
+++ main.py
@@ -0,0 +1,7 @@
+r = int(input("Enter the radius of circle : "))
+def me(r):
+    circumference = 2*(22/7)*r
+    area = r*(22/7)**2
+    print("The circumference of the circle is",circumference,".")
+    print("The area of the circle is",area,".")
+me()
